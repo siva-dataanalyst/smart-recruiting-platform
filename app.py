48a829924ebb283ed19aceac8eca55702163bc82
Siva Rama Krishna Vellaturi <sivaramakrishnavellaturi1824@gmail.com>
2026-04-23 09:21:55 +0530

    Phase 2 - Job Description and Skill Matching completed

--- app.py
+++ app.py
@@ -44,3 +44,57 @@ if uploaded_file is not None:
     st.info(f"📊 Total characters extracted: {len(extracted_text)}")
 else:
     st.warning("⬆️ Please upload a resume PDF to get started")
+
+
+
+# Phase 2- Job Description Input & Skill Matching
+st.markdown("---")
+st.subheader("📝 Phase 2 - Job Description & Skill Matching")
+
+
+# Job Description Input
+job_description=st.text_area(
+    "Paste Job Description Here",
+    height=200,
+    placeholder="Paste the job description here..."
+)
+
+
+# Let recruiter enter required skills
+skills_input=st.text_input(
+    "Enter required skills (comma separated)",
+    placeholder="python, sql, power bi, excel..."
+)
+
+# Funtion to match skills
+def match_skills(resume_text,skills_list):
+    resume_text=resume_text.lower()
+    matched=[]
+    unmatched=[]
+    for skill in skills_list:
+        if skill in resume_text:
+            matched.append(skill)
+        else:
+            unmatched.append(skill)
+    return matched , unmatched
+# Show results
+if uploaded_file is not None and skills_input:
+    # Convert input to list
+    skills_list=[skill.strip().lower() for skill in skills_input.split(",") if skill.strip()]
+    matched_skills , unmatched_skills = match_skills(
+        extracted_text,skills_list
+    )
+    st.markdown("---")
+    st.subheader("📊 Skill Match Results")
+
+    # Match score
+    score=len(matched_skills)/len(skills_list) * 100
+
+    # Display score
+    st.info(f"📊 Match Score: {score:.1f}%")
+
+    # Display Matched skills
+    st.success(f"✅ Matched Skills: {', '.join(matched_skills) if matched_skills else 'None'}")
+
+    # Display unmatched skills
+    st.error(f"❌ Missing Skills: {', '.join(unmatched_skills) if unmatched_skills else 'None'}")
